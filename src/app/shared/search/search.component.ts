6f20d064ff1414777ef09cb4900adb1f9b8677c0
thiago barbosa <thiagon16@hotmail.com>
2025-03-16 21:55:34 -0300

    feat: add search component

---
+++ src/app/shared/search/search.component.ts
@@ -0,0 +1,12 @@
+import { NgOptimizedImage } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search',
+  imports: [NgOptimizedImage],
+  templateUrl: './search.component.html',
+  styleUrl: './search.component.css'
+})
+export class SearchComponent {
+
+}

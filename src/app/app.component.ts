7429e2499bb92864b4d1ef1cd51605f4610259d5
thiago barbosa <thiagon16@hotmail.com>
2025-03-16 21:55:58 -0300

    feat: add styles global

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./shared/header/header.component";
+import { SearchComponent } from "./shared/search/search.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, SearchComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/data/posts.ts
@@ -0,0 +1,9 @@
+interface Post {
+  id: number
+  title: string
+  imageUrl: string
+  content: string
+  publishedAt: Date
+}
+
+const posts = []
